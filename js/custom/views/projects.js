6e87cb5617ca01a6d25d22529cbe50d33303b39a
clydedsouza <clydedsouza@users.noreply.github.com>
2020-10-15 09:48:12 +0000

    Automated release from Azure DevOps - Release 20201015.28

---
+++ js/custom/views/projects.js
@@ -0,0 +1,9 @@
+﻿function projectViewPageLoad() { 
+    projectRepeaterViewPreSwitchTemplate("https://api.clydedsouza.net/allprojects.json", "projectsPartial");
+}
+
+
+function projectViewPreSwitchTemplate() {
+    switchTemplate("projectsPartial", {});
+}
+
